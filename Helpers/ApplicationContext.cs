9f3326018fbcacb44024f05ffcd2738cb0f73bea
Jakub <jakub.popiolek@informed.com>
2024-10-30 12:18:50 +0000

    changing codebase to use repository model

--- Helpers/ApplicationContext.cs
+++ Helpers/ApplicationContext.cs
@@ -5,22 +5,13 @@ namespace EvApplicationApi.Helpers;
 
 public class ApplicationContext : DbContext
 {
-    protected readonly IConfiguration Configuration;
-
-    public ApplicationContext(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
-    }
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<ApplicationItem>().HasOne(applicationItem => applicationItem.Address);
     }
 
-    public DbSet<ApplicationItem> ApplicationItems { get; set; } = null!;
+    public DbSet<ApplicationItem> ApplicationItems { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using EvApplicationApi.Helpers;
+using EvApplicationApi.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,8 +12,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiDatabase"))
+    options.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase"))
 );
+
+builder.Services.AddScoped<IApplicationsRepository, ApplicationRepository>();
 
 var app = builder.Build();
 
